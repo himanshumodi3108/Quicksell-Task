dba0f572b1e7938b6db4ecb6a7bfcc7fdc84292b
himanshumodi2001 <31august2001@gmail.com>
2024-10-23 16:57:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import DashBoard from "./components/DashBoard/DashBoard";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllData } from "./Actions/DataAction";
+
+const App = () => {
+  const dispatch = useDispatch();
+  // eslint-disable-next-line no-unused-vars
+  const { allTickets } = useSelector((state) => state.DataReducer);
+
+  useEffect(() => {
+    dispatch(fetchAllData());
+  }, [dispatch]);
+
+  return (
+    <div style={{ paddingTop: "10px" }}>
+      <NavBar />
+      <hr style={{ marginTop: "10px" }} />
+      <DashBoard />
+    </div>
+  );
+};
+
+export default App;
